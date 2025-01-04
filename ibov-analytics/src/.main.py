1a673b053ab8545b638518d2127e475f8409aa78
flaviarubira <flavia.rubira@icloud.com>
2025-01-04 01:20:34 -0300

    Update .main.py

--- ibov-analytics/src/.main.py
+++ ibov-analytics/src/.main.py
@@ -1,1 +1,16 @@
+import sys
+import os
 
+sys.path.append(os.path.abspath("src"))
+
+from data_collector import get_ibov_data
+
+if _name_ == "_main_":
+    # Código principal
+    from data_collector import get_ibov_data
+
+    def main():
+        data = get_ibov_data("2023-01-01", "2024-01-01")
+        print(data.head())
+
+    main()
